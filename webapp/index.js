65c5698304cefdc6e9061476e7d10e79454fc3b5
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-24 13:46:55 +0300

    step 4: add two-way binding

--- webapp/index.js
+++ webapp/index.js
@@ -1,18 +1,21 @@
 sap.ui.require(
-  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
-  function (Text, JSONModel) {
+  ["sap/m/Text", "sap/ui/model/json/JSONModel", "sap/ui/core/mvc/XMLView"],
+  function (Text, JSONModel, XMLView) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
     sap.ui.getCore().attachInit(function () {
       const oModel = new JSONModel({
-        greeting: "Hello world!",
+        panelHeaderText: "Data Binding Basics",
+        firstName: "Harry",
+        lastName: "Hawk",
+        enabled: true,
       });
 
       sap.ui.getCore().setModel(oModel);
 
-      new Text({
-        text: "{/greeting}",
+      new XMLView({
+        viewName: "sap.ui.demo.db.view.App",
       }).placeAt("content");
     });
   }
