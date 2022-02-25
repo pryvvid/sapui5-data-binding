731621975390ddd1c943fefd552bb4d95c6c3c18
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-25 15:24:58 +0300

    step 15: add aggregation binding using factory function

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,8 +5,16 @@ sap.ui.define(
     "sap/ui/core/Locale",
     "sap/ui/core/LocaleData",
     "sap/ui/model/type/Currency",
+    "sap/m/ObjectAttribute",
   ],
-  function (Controller, mobileLibrary, Locale, LocaleData, Currency) {
+  function (
+    Controller,
+    mobileLibrary,
+    Locale,
+    LocaleData,
+    Currency,
+    ObjectAttribute
+  ) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.wt.controller.App", {
@@ -37,6 +45,31 @@ sap.ui.define(
         const sPath = oContext.getPath();
         const oProductDetailPanel = this.byId("productDetailsPanel");
         oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+      },
+      productListFactory: function (sId, oContext) {
+        let oUIControl;
+
+        // Decide based on the data which dependent to clone
+        if (
+          oContext.getProperty("UnitsInStock") === 0 &&
+          oContext.getProperty("Discontinued")
+        ) {
+          oUIControl = this.byId("productSimple").clone(sId);
+        } else {
+          oUIControl = this.byId("productExtended").clone(sId);
+
+          if (oContext.getProperty("UnitsInStock") < 1) {
+            oUIControl.addAttribute(
+              new ObjectAttribute({
+                text: {
+                  path: "i18n>outOfStock",
+                },
+              })
+            );
+          }
+        }
+
+        return oUIControl;
       },
     });
   }
